e9c8cb118067bbd875d125a91a64297d962f36c9
Yanaredko <136071376+Yanaredko@users.noreply.github.com>
2023-12-01 17:07:32 +0100

    feedback changes

--- src/middlewares/validateContact.js
+++ src/middlewares/validateContact.js
@@ -1,11 +1,15 @@
-const validateContact = (schema) => (req, res, next ) => {
+const HttpError = require('../helpers/httpError');
+
+const validateContact = (schema) => (req, res, next) => {
   try {
     const { name, email, phone } = req.body;
     const validationResult = schema.validate({ name, email, phone });
 
     if (validationResult.error) {
-      throw new Error()
+       const errorMessage = validationResult.error.details[0].message;
+      throw new HttpError(400, errorMessage);
     }
+    next();
   } catch(e) {
     next(e);
   }
